4269e625990b518e72411bb29d21e9c7013d69eb
Malte Tammena <malte.tammena@gmx.de>
2020-08-07 17:27:37 +0200

    Some finalisations, simple doc

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# lcd-avr-rust-example
+# lcd-avr
 
 Example communication between an the PC, the ATmega328p and an LCD.
 
 The user can send strings over a serial connection that will be displayed on the
 connected LCD display. The strings must end with '\0', the zero byte.
+
+Do the wiring according to these and have a go at it yourself!
+![Foto of the
+thing](https://raw.githubusercontent.com/MalteT/lcd-avr/main/static/foto.jpg)
+![Wiring stuff](https://raw.githubusercontent.com/MalteT/lcd-avr/main/static/lcd-sketch-fritzing_bb.svg)
+
+License: MIT
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@
 //!
 //! The user can send strings over a serial connection that will be displayed on the
 //! connected LCD display. The strings must end with '\0', the zero byte.
+//!
+//! Do the wiring according to these and have a go at it yourself!
+//! ![Foto of the
+//! thing](https://raw.githubusercontent.com/MalteT/lcd-avr/main/static/foto.jpg)
+//! ![Wiring stuff](https://raw.githubusercontent.com/MalteT/lcd-avr/main/static/lcd-sketch-fritzing_bb.svg)
 
 use atmega328p_hal::{atmega328p::Peripherals, clock, delay::Delay, prelude::*, usart::Usart0};
 use nb::block;
